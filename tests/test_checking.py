73b92516c5fcc9938301f4041ae202794f7b0eb5
rabbit19981023 <rabbit19981023@gmail.com>
2023-04-26 17:57:33 +0000

    add more test cases and remove impossible cases for `checking` module

--- tests/test_checking.py
+++ tests/test_checking.py
@@ -5,18 +5,36 @@ from yomigana_ebook.constants import ALL_HIRA, ALL_KATA
 from yomigana_ebook.checking import is_hira_only, is_kata_only, is_kanji_only
 
 
-def test_is_hira_only():
+@pytest.mark.parametrize(
+    "test_case, text, hira, expected",
+    [
+        ("all hira", ALL_HIRA, ALL_HIRA, True),
+        ("all kata", ALL_KATA, ALL_HIRA, False),
+        ("all kanji", "漢字", "かんじ", False),
+        ("contains kanji", "感じ", "かんじ", False),
+    ]
+)
+def test_is_hira_only(test_case: str, text: str, hira: str, expected: bool):
     for char in ALL_HIRA:
         if not "HIRAGANA" in unicodedata.name(char):
             assert False
-    assert is_hira_only(ALL_HIRA, ALL_HIRA)
+    assert is_hira_only(text, hira) == expected
 
 
-def test_is_kata_only():
+@pytest.mark.parametrize(
+    "test_case, text, kata, expected",
+    [
+        ("all hira", ALL_HIRA, ALL_KATA, False),
+        ("all kata", ALL_KATA, ALL_KATA, True),
+        ("all kanji", "漢字", "カンジ", False),
+        ("contains kanji", "感じ", "カンジ", False),
+    ]
+)
+def test_is_kata_only(test_case: str, text: str, kata: str, expected: bool):
     for char in ALL_KATA:
         if not "KATAKANA" in unicodedata.name(char):
             assert False
-    assert is_kata_only(ALL_KATA, ALL_KATA)
+    assert is_kata_only(text, kata) == expected
 
 
 @pytest.mark.parametrize(
@@ -27,7 +45,6 @@ def test_is_kata_only():
         ("all kata", ALL_KATA, False),
         ("contains hira", "感じ", False),
         ("contains `々`", "時々", True),
-        ("contains mark", "漢字。", False)
     ]
 )
 def test_is_kanji_only(test_case: str, kanji: str, expected: bool):
