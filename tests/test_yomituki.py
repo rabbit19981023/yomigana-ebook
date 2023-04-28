162103df3fac111933c83f2e47d4f7616d1a9683
rabbit19981023 <rabbit19981023@gmail.com>
2023-04-28 11:07:20 +0000

    add more test cases for `yomituki_word()` and roughly implements it

--- tests/test_yomituki.py
+++ tests/test_yomituki.py
@@ -1,6 +1,7 @@
 import pytest
 
-from yomigana_ebook.yomituki import yomituki_sentence
+from yomigana_ebook.constants import ALL_HIRA, ALL_KATA, ALL_NUMBER
+from yomigana_ebook.yomituki import yomituki_sentence, yomituki_word
 
 
 @pytest.mark.parametrize(
@@ -11,3 +12,20 @@ from yomigana_ebook.yomituki import yomituki_sentence
 )
 def test_yomituki_sentence(test_case: str, sentence: str, expected: str):
     assert yomituki_sentence(sentence) == expected
+
+
+@pytest.mark.parametrize(
+    "test_case, surface, kata, expected",
+    [
+        ("mark", "。", "*", "。"),
+        ("all hira", ALL_HIRA, ALL_KATA, ALL_HIRA),
+        ("all kata", ALL_KATA, ALL_KATA, ALL_KATA),
+        ("all kanji", "漢字", "カンジ", "<ruby>漢字<rt>かんじ</rt></ruby>"),
+        ("all number", ALL_NUMBER, "dummy kata", ALL_NUMBER),
+        ("kanji + hira", "見上げて", "ミアゲテ", "<ruby>見上<rt>みあ</rt></ruby>げて"),
+        ("hira + kanji", "うれし涙", "ウレシナミダ", "うれし<ruby>涙<rt>なみだ</rt></ruby>"),
+        ("kanji + hira + kanji + hira", "思い出した", "オモイダシタ", "<ruby>思<rt>おも</rt></ruby>い<ruby>出<rt>だ</rt></ruby>した"),
+    ]
+)
+def test_yomituki_word(test_case: str, surface: str, kata: str, expected: str):
+    assert yomituki_word(surface, kata) == expected
--- yomigana_ebook/yomituki.py
+++ yomigana_ebook/yomituki.py
@@ -1,6 +1,9 @@
-from yomigana_ebook.analyzer import Analyzer
+from typing import Tuple
+from os.path import commonprefix
+
+from yomigana_ebook.analyzer import Analyzer, Morpheme
 from yomigana_ebook.converter import kata2hira
-from yomigana_ebook.checking import is_mark, is_hira_only, is_kata_only, is_kanji_only, is_number_only
+from yomigana_ebook.checking import is_mark, is_hira_only, is_kata_only, is_kanji_only, is_number_only, is_kanji
 
 
 def yomituki_sentence(sentence: str) -> str:
@@ -27,11 +30,51 @@ def yomituki_word(surface: str, kata: str) -> str:
     if is_number_only(surface):
         return surface
 
-    # TODO:
-    # deal with compound word
+    # yomituki for:
+    # hira + kanji: うれし涙
+    # kanji + hira: 見上げて
+    (prefix, middle, suffix) = cut_by_hira(surface, hira)
+    if is_kanji_only(middle.surface):
+        return f"{prefix}{ruby_wrap(middle.surface, middle.reading)}{suffix}"
 
-    return ""
+    # yomituki for
+    # kanji + hira + kanji + hira: 思い出した
+    hira_index_surface = 0
+    hira_in_surface = ""
+    for i, char in enumerate(middle.surface[::-1]):
+        if not is_kanji(char):
+            hira_index_surface = -i - 1
+            hira_in_surface = middle.surface[hira_index_surface]
+
+    hira_index_reading = 0
+    for i, char in enumerate(middle.reading[::-1]):
+        if char == hira_in_surface:
+            hira_index_reading = -i - 1
+
+    return f"{prefix}" \
+        f"{ruby_wrap(middle.surface[:hira_index_surface], middle.reading[:hira_index_reading])}" \
+        f"{hira_in_surface}" \
+        f"{ruby_wrap(middle.surface[hira_index_surface+1:], middle.reading[hira_index_reading+1:])}" \
+        f"{suffix}"
 
 
 def ruby_wrap(kanji: str, hira: str) -> str:
     return f"<ruby>{kanji}<rt>{hira}</rt></ruby>"
+
+
+def cut_by_hira(surface: str, hira: str) -> Tuple[str, Morpheme, str]:
+    prefix = find_common_prefix(surface, hira)
+    suffix = find_common_suffix(surface, hira)
+    middle = Morpheme(
+        surface.removeprefix(prefix).removesuffix(suffix),
+        hira.removeprefix(prefix).removesuffix(suffix)
+    )
+    return (prefix, middle, suffix)
+
+
+def find_common_prefix(str1: str, str2: str) -> str:
+    return commonprefix((str1, str2))
+
+
+def find_common_suffix(str1: str, str2: str) -> str:
+    return commonprefix((str1[::-1], str2[::-1]))[::-1]
