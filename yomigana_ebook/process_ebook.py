af7faf3ada87a60d2c4a088780a30607e303497f
rabbit19981023 <rabbit19981023@gmail.com>
2023-06-02 19:09:24 +0000

    parallel processing of the ebook using `concurrent.futures.ProcessPoolExecutor` to improve efficiency

--- yomigana_ebook/process_ebook.py
+++ yomigana_ebook/process_ebook.py
@@ -1,6 +1,7 @@
 from warnings import filterwarnings
 from typing import IO
 from zipfile import ZipFile, ZIP_DEFLATED
+from concurrent.futures import Future, ProcessPoolExecutor, as_completed
 
 from bs4 import BeautifulSoup, NavigableString, Tag, XMLParsedAsHTMLWarning
 from yomigana_ebook.yomituki import yomituki_sentence
@@ -13,19 +14,33 @@ def process_ebook(reader: IO[bytes], writer: IO[bytes]):
     with (
         ZipFile(reader, "r") as zip_reader,
         ZipFile(writer, "w", ZIP_DEFLATED) as zip_writer,
+        ProcessPoolExecutor() as executor,
     ):
+        tasks: list[Future[tuple[str, bytes]]] = []
+
         for file in zip_reader.namelist():
             with zip_reader.open(file) as file_reader:
-                if file.endswith(("xhtml", "html")):
-                    soup = BeautifulSoup(file_reader.read(), "lxml")
+                content = file_reader.read()
 
-                    for child in soup.children:
-                        process_tag(child)  # type: ignore
+            if file.endswith(("xhtml", "html")):
+                task = executor.submit(process_html, file, content)
+            else:
+                task = executor.submit(process_resources, file, content)
 
-                    zip_writer.writestr(file, soup.encode(formatter=None))  # type: ignore
+            tasks.append(task)
 
-                else:
-                    zip_writer.writestr(file, file_reader.read())
+        for task in as_completed(tasks):
+            file, processed_content = task.result()
+            zip_writer.writestr(file, processed_content)
+
+
+def process_html(file: str, content: bytes):
+    soup = BeautifulSoup(content, "lxml")
+
+    for child in soup.children:
+        process_tag(child)  # type: ignore
+
+    return file, soup.encode(formatter=None)  # type: ignore
 
 
 def process_tag(tag: Tag):
@@ -39,3 +54,7 @@ def process_tag(tag: Tag):
     if hasattr(tag, "children"):
         for child in tag.children:
             process_tag(child)  # type: ignore
+
+
+def process_resources(file: str, content: bytes):
+    return file, content
